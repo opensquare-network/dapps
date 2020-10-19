2c3a17a152a2986675c275d8be810c9097d0b955
Yongfeng LI <wliyongfeng@gmail.com>
2020-10-19 18:36:37 +0800

    Play react-simplemde-editor

--- packages/app/src/pages/Bounties/index.js
+++ packages/app/src/pages/Bounties/index.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { testState } from "@recoil/atoms";
 import { useRecoilState } from 'recoil';
+import "easymde/dist/easymde.min.css";
+import SimpleMDEReact from "react-simplemde-editor";
 
 export default function() {
   const [text, setText] = useRecoilState(testState);
+  const [value, setValue] = useState('');
+
+  console.log('value', value);
 
   const onChange = (event) => {
     setText(event.target.value);
   };
 
   return (
-    <div style={{textAlign: 'center'}}>
+    <div>
       <input type="text" value={text} onChange={onChange} />
       <br />
       Echo: {text}
+
+      <div style={{ width: '1080px' }}>
+        <SimpleMDEReact
+          className={""}
+          label="Markdown Editor"
+          value={value}
+          onChange={setValue}
+        />
+      </div>
     </div>
   );
 }
