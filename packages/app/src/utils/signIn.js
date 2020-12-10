253262b14a7eafe40818e0c7d367ceb1e923c0b6
Yongfeng LI <wliyongfeng@gmail.com>
2020-12-11 00:11:29 +0800

    Call web3Accounts before getting injector

--- packages/app/src/utils/signIn.js
+++ packages/app/src/utils/signIn.js
@@ -1,8 +1,15 @@
 import { getApi } from "@services/api";
-import { web3Enable, web3FromAddress } from '@polkadot/extension-dapp'
+import { web3Accounts, web3Enable, web3FromAddress } from '@polkadot/extension-dapp'
 
 export async function signIn(account) {
   await web3Enable('OpenSquare')
+  const allAccounts = await web3Accounts()
+  const addressesInExtension = allAccounts.map(account => account.address)
+  if (!addressesInExtension.includes(account.extensionAddress)) {
+    localStorage.removeItem('currentAccount')
+    return
+  }
+
   const injector = await web3FromAddress(account.extensionAddress)
   const api = await getApi()
   api.setSigner(injector.signer)
