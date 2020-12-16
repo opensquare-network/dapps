cff249e9235dc35ce2a0d2044e34bd682307009e
qiyisi <qiyisi@qq.com>
2020-12-16 18:50:30 +0800

    change resolve button when in block

--- packages/app/src/pages/BountyDetail/ResolveButton.js
+++ packages/app/src/pages/BountyDetail/ResolveButton.js
@@ -8,9 +8,8 @@ import { nowAddressSelector } from "@store/reducers/accountSlice";
 import { addFlashToast, toastType } from "@store/reducers/toastSlice";
 import { bountySelector, fetchBounty } from "../../store/reducers/bountySlice";
 
-import apiTypes from "@services/types"
+import apiTypes from "@services/types";
 import styled from "styled-components";
-
 
 const ListModal = styled(Modal)`
   .select-content {
@@ -20,91 +19,104 @@ const ListModal = styled(Modal)`
       font-size: 1.1em;
 
       &:hover {
-        background: #FBFBFB;
+        background: #fbfbfb;
       }
     }
   }
-`
+`;
 
 export default function () {
-  const nowAddress = useSelector(nowAddressSelector)
-  const [showResolveAndRemarkModel, setShowResolveAndRemarkModel] = useState(false)
-  const { bountyId } = useParams()
-  const dispatch = useDispatch()
-  const bounty = useSelector(bountySelector)
+  const nowAddress = useSelector(nowAddressSelector);
+  const [showResolveAndRemarkModel, setShowResolveAndRemarkModel] = useState(
+    false
+  );
+  const [isSuccess, setIsSuccess] = useState(false);
+  const { bountyId } = useParams();
+  const dispatch = useDispatch();
+  const bounty = useSelector(bountySelector);
 
-  const isFunder = bounty?.creator === nowAddress
-  const isSubmitted = bounty?.state?.state === 'Submitted'
-  const isResolved = bounty?.state?.state === 'Resolved'
+  const isFunder = bounty?.creator === nowAddress;
+  const isSubmitted = bounty?.state?.state === "Submitted";
+  const isResolved = bounty?.state?.state === "Resolved";
 
   const resolveBountyAndRemark = async (bountyId, option) => {
-    const api = await getApi()
-    const unsub = await api.tx.osBounties.resolveBountyAndRemark(bountyId, option)
+    const api = await getApi();
+    const unsub = await api.tx.osBounties
+      .resolveBountyAndRemark(bountyId, option)
       .signAndSend(nowAddress, async ({ events = [], status }) => {
-        console.log('status', status)
-        dispatch(fetchBounty(bountyId))
-
-        if (status.isInBlock) {
-          dispatch(addFlashToast(toastType.INFO, 'Extrinsic inBlock'))
+        if (status.isFinalized) {
+          dispatch(fetchBounty(bountyId));
+          unsub();
         }
+
+        if (!status.isInBlock) {
+          return;
+        }
+
+        dispatch(addFlashToast(toastType.INFO, "Extrinsic inBlock"));
 
         for (const item of events) {
-          console.log('events', events)
-          const { event } = item
-          const method = event.method
-          const data = event.data.toJSON()
+          const { event } = item;
+          const method = event.method;
+          const data = event.data.toJSON();
 
-          if ('Resolve' === method && status.isFinalized) {
-            const [bountyId] = data
-            console.log(`Resolve bounty ${bountyId}`)
-            dispatch(addFlashToast(toastType.SUCCESS, 'Bounty resolved'))
+          if ("Resolve" === method) {
+            const [bountyId] = data;
+            console.log(`Resolve bounty ${bountyId}`);
+            dispatch(addFlashToast(toastType.SUCCESS, "Bounty resolved"));
+            dispatch(fetchBounty(bountyId));
+            setIsSuccess(true);
           }
         }
-
-        if (status.isFinalized) {
-          unsub()
-        }
-      })
-  }
+      });
+  };
 
   return (
     <>
-      { ((isSubmitted || isResolved) && isFunder) &&
-          <Button primary onClick={() => {
-            setShowResolveAndRemarkModel(true)
-          }} disabled={isResolved}>
-            { isResolved ? 'Resolved' : 'Resolve and Remark' }
-          </Button>
-      }
+      {(isSubmitted || isResolved) && isFunder && (
+        <Button
+          primary
+          onClick={() => {
+            setShowResolveAndRemarkModel(true);
+          }}
+          disabled={isResolved || isSuccess}
+        >
+          {isResolved || isSuccess ? "Resolved" : "Resolve and Remark"}
+        </Button>
+      )}
 
       <ListModal
         size="mini"
         open={showResolveAndRemarkModel}
         onClose={() => {
-          setShowResolveAndRemarkModel(false)
+          setShowResolveAndRemarkModel(false);
         }}
       >
         <Modal.Header>Resove and Remark</Modal.Header>
         <Modal.Content className="select-content">
-          { apiTypes ?
-            (
-              <ol>
-                {(apiTypes.BountyRemarkCollaborationResult._enum || []).map(option => {
+          {apiTypes ? (
+            <ol>
+              {(apiTypes.BountyRemarkCollaborationResult._enum || []).map(
+                (option) => {
                   return (
-                    <li key={option} onClick={() => {
-                      resolveBountyAndRemark(bountyId, option)
-                      setShowResolveAndRemarkModel(false)
-                    }}>
+                    <li
+                      key={option}
+                      onClick={() => {
+                        resolveBountyAndRemark(bountyId, option);
+                        setShowResolveAndRemarkModel(false);
+                      }}
+                    >
                       <span>{option}</span>
                     </li>
-                  )
-                })}
-              </ol>
-            )
-            : <span>Api types not available</span>
-          }
+                  );
+                }
+              )}
+            </ol>
+          ) : (
+            <span>Api types not available</span>
+          )}
         </Modal.Content>
       </ListModal>
     </>
-  )
+  );
 }
