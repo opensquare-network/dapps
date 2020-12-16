9572d2d448a4c674a11093ddb327629e1fb54ba5
qiyisi <qiyisi@qq.com>
2020-12-16 10:32:16 +0800

    fix amount and success info

--- packages/app/src/pages/Profile/TransitionModal.js
+++ packages/app/src/pages/Profile/TransitionModal.js
@@ -2,10 +2,12 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { Button, Header, Modal, Dropdown, Input } from "semantic-ui-react";
-import { web3Accounts, web3Enable } from "@polkadot/extension-dapp";
+import BigNumber from "bignumber.js";
 
 import { getApi } from "@services/api";
+import { web3Accounts, web3Enable } from "@polkadot/extension-dapp";
 import { nowAddressSelector } from "@store/reducers/accountSlice";
+import { osnPrecisionSelector } from "@store/reducers/chainSlice";
 import { addFlashToast, toastType } from "@store/reducers/toastSlice";
 
 const Wrapper = styled.div`
@@ -21,11 +23,13 @@ const Wrapper = styled.div`
 export default function () {
   const dispatch = useDispatch();
   const nowAddress = useSelector(nowAddressSelector);
+  const precision = useSelector(osnPrecisionSelector);
 
   const [open, setOpen] = React.useState(false);
   const [accounts, setAccounts] = React.useState([]);
   const [address, setAddress] = React.useState("");
   const [amount, setAmount] = React.useState("");
+  const [isButtonDisabled, setIsButtonDisabled] = React.useState(false);
 
   useEffect(() => {
     const getAllAccounts = async () => {
@@ -46,25 +50,45 @@ export default function () {
     if (open === false) {
       setAddress("");
       setAmount("");
+      setIsButtonDisabled(false);
     }
   }, [open]);
 
   const transfer = async () => {
+    if (!address) {
+      dispatch(addFlashToast(toastType.ERROR, "Please select an address"));
+      return;
+    }
+    if (!amount) {
+      dispatch(addFlashToast(toastType.ERROR, "Please enter an amount"));
+      return;
+    }
+    setIsButtonDisabled(true);
+    const realAmount = new BigNumber(amount)
+      .multipliedBy(Math.pow(10, precision))
+      .toNumber();
     const api = await getApi();
-    await api.tx.currencies
-      .transfer(address, "OSN", Number(amount))
+    const unsub = await api.tx.currencies
+      .transfer(address, "OSN", realAmount)
       .signAndSend(nowAddress, async ({ events = [], status }) => {
-        console.log(events, status);
+        if (status.isFinalized) {
+          unsub();
+        }
+        if (!status.isInBlock) {
+          return;
+        }
+        dispatch(addFlashToast(toastType.INFO, "Extrinsic inBlock"));
         for (const item of events) {
           const { event } = item;
           const method = event.method;
-          // const data = event.data.toJSON()
-          if (method === "Transfer") {
+          if (method === "Transferred") {
             dispatch(addFlashToast(toastType.SUCCESS, "Transfer success"));
             setOpen(false);
             return;
           }
         }
+        dispatch(addFlashToast(toastType.ERROR, "Transfer error"));
+        setIsButtonDisabled(false);
       });
   };
 
@@ -107,7 +131,12 @@ export default function () {
         </Modal.Content>
         <Modal.Actions>
           <Button secondary content="Cancel" onClick={() => setOpen(false)} />
-          <Button primary content="Confirm" onClick={transfer} />
+          <Button
+            primary
+            content="Confirm"
+            onClick={transfer}
+            disabled={isButtonDisabled}
+          />
         </Modal.Actions>
       </Modal>
     </Wrapper>
